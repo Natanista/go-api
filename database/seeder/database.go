11793b84b1ce814f25b303d9a29e6440c5c460bc
Natan <natanista@pop-os.localdomain>
2022-04-29 01:10:35 -0300

    add database uri

--- database/seeder/database.go
+++ database/seeder/database.go
@@ -3,7 +3,7 @@ package database
 import (
 	"fmt"
 	"log"
-	"os"
+	// "os"
 	"time"
 
 	"github.com/natanista/go-api/database/migrations"
@@ -14,16 +14,17 @@ import (
 var db *gorm.DB
 
 func StartDB() {
-	host := os.Getenv("DATABASE_HOST")
-	port := os.Getenv("DATABASE_PORT")
-	user := os.Getenv("DATABASE_USER")
-	dbname := os.Getenv("DATABASE_DBNAME")
-	password := os.Getenv("DATABASE_PASSWORD")
+	// host := os.Getenv("DATABASE_HOST")
+	// dialect := os.Getenv("DATABASE_DIALECT")
+	// port := os.Getenv("DATABASE_PORT")
+	// user := os.Getenv("DATABASE_USER")
+	// dbname := os.Getenv("DATABASE_DBNAME")
+	// password := os.Getenv("DATABASE_PASSWORD")
 
-	str := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=disable password=%s", host, port, user, dbname, password)
+	// str := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=disable password=%s", host, port, user, dbname, password)
 
 	
-	database, err := gorm.Open(postgres.Open(str), &gorm.Config{})
+	database, err := gorm.Open(postgres.Open("postgres://vtvsbfqobfssqw:edee9aa648b04a7940144994afe1925a1c83fdcf876689394759e272f4207fe5@ec2-34-194-73-236.compute-1.amazonaws.com:5432/d2n1bq3i9rb6vt"), &gorm.Config{})
 
 	if err != nil {
 		fmt.Println("Could not connect to the Postgres Database")
